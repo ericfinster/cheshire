8b1f32a8f9e4c689e6e1b5bb3b1fe21ab4e2239b
Eric Finster <ericfinster@gmail.com>
2020-07-27 11:01:21 +0200

    new syntax working

--- lib/cheshire.ml
+++ lib/cheshire.ml
@@ -2,8 +2,44 @@
  * Cheshire.ml - Top Level Module for Cheshire
  *)
 
-type 'a blorp =
-  | Yes of 'a
-  | No of string
+open Option
+
+module OptionMnd =
+struct
+  
+  let product o o' =
+    match (o , o') with
+    | Some x , Some y -> Some (x , y)
+    |  _ -> None
+      
+
+  module Syntax =
+  struct
+    let (let+) x f = map f x
+    let (and+) o o' = product o o'
+    let (let*) m f = bind m f
+  end
+  
+end
+
+let head_opt l =
+  match l with 
+  | [] -> None
+  | x::_ -> Some x
+
+let tail_opt l =
+  match l with
+  | [] -> None
+  | _::xs -> Some xs 
+
+open OptionMnd.Syntax
+       
+let test l =
+  let* x = head_opt l in
+  let* xs = tail_opt l in
+  let* y = head_opt xs in
+  Some (x + y)
+  
+
 
 
