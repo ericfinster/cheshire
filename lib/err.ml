9048b55f2fea655bd866313745a45244634087de
Eric Finster <ericfinster@gmail.com>
2020-07-27 11:08:39 +0200

    simple error monad

---
+++ lib/err.ml
@@ -0,0 +1,36 @@
+(*
+ * Err.ml - An Error Monad
+ *)
+
+type 'a err =
+  | Ok of 'a
+  | Fail of string
+
+module Err =
+struct
+
+  (* Bind for the error monad *)
+  let ( >>= ) m f =
+    match m with
+    | Ok a -> f a
+    | Fail s -> Fail s
+
+  let product e e' =
+    match e , e' with
+    | Ok x , Ok y -> Ok (x , y)
+    | Fail s , _ -> Fail s
+    | _ , Fail s' -> Fail s'
+
+  let map e f =
+    match e with
+    | Ok x -> Ok (f x)
+    | Fail s -> Fail s
+    
+  module Syntax =
+  struct
+    let (let+) e f = map e f
+    let (and+) e e' = product e e'
+    let (let*) m f = m >>= f
+  end
+  
+end
